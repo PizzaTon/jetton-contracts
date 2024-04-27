f2f8ad9621579aea0565a84aea91ff8cd96fca23
mhbdev <53684884+mhbdev@users.noreply.github.com>
2024-04-27 12:12:48 +0330

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+# PizzaTon Jetton Contracts on TON blockchain
+
+Discoverable Jettons Wallets based on [TEP-89](https://github.com/ton-blockchain/TEPs/blob/master/text/0089-jetton-wallet-discovery.md)
+
+CA on Mainnet:
+`EQAgotSkX06MIW-A0ni5yKqeNlwc3nASnbO1dwGo-kwpg2Zg`
+
+[TonViewer](https://tonviewer.com/EQAgotSkX06MIW-A0ni5yKqeNlwc3nASnbO1dwGo-kwpg2Zg)
+[TonScan](https://tonscan.org/address/EQAgotSkX06MIW-A0ni5yKqeNlwc3nASnbO1dwGo-kwpg2Zg)
---
+++ ptj/tests/JettonMinter.spec.ts
@@ -0,0 +1,40 @@
+import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
+import { Cell, toNano } from '@ton/core';
+import { JettonMinter } from '../wrappers/JettonMinter';
+import '@ton/test-utils';
+import { compile } from '@ton/blueprint';
+
+describe('JettonMinter', () => {
+    let code: Cell;
+
+    beforeAll(async () => {
+        code = await compile('JettonMinter');
+        console.log(code.toBoc().toString('base64'));
+    });
+
+    let blockchain: Blockchain;
+    let deployer: SandboxContract<TreasuryContract>;
+    let jettonMinter: SandboxContract<JettonMinter>;
+
+    // beforeEach(async () => {
+    //     blockchain = await Blockchain.create();
+
+    //     jettonMinter = blockchain.openContract(JettonMinter.createFromConfig({}, code));
+
+    //     deployer = await blockchain.treasury('deployer');
+
+    //     const deployResult = await jettonMinter.sendDeploy(deployer.getSender(), toNano('0.05'));
+
+    //     expect(deployResult.transactions).toHaveTransaction({
+    //         from: deployer.address,
+    //         to: jettonMinter.address,
+    //         deploy: true,
+    //         success: true,
+    //     });
+    // });
+
+    it('should deploy', async () => {
+        // the check is done inside beforeEach
+        // blockchain and jettonMinter are ready to use
+    });
+});
